eebb8803c3909b032359ae570080e972716b6632
Totatitu <totatitu1995@gmail.com>
2014-12-10 13:53:29 +0100

    ok

--- accueil.php
+++ accueil.php
@@ -1,7 +1,10 @@
 <html>
     <head>
         <meta charset="UTF8">
-        
+        <title>Crowdfunding</title>
+        <center>
+            <img src="img/crowdfunding.jpg">
+        </center>
         
     </head>
     <body>
